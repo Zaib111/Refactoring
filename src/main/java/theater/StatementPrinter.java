87cfbc8c1034a85bb53a64752ed22de5158933c3
Shahmeer Khan <shahmeerkh2006@gmail.com>
2025-11-15 00:02:00 -0500

    Task 2.1

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -31,8 +31,6 @@ public class StatementPrinter {
 
         for (Performance performance : getInvoice().getPerformances()) {
 
-            final int thisAmount = getAmount(performance);
-
             // add volume credits
             volumeCredits += Math.max(performance.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
             // add extra credit for every five comedy attendees
@@ -41,8 +39,8 @@ public class StatementPrinter {
             }
             // print line for this order
             result.append(String.format("  %s: %s (%s seats)%n", getPlay(performance).getName(),
-                    frmt.format(thisAmount / Constants.PERCENT_FACTOR), performance.getAudience()));
-            totalAmount += thisAmount;
+                    frmt.format(getAmount(performance) / Constants.PERCENT_FACTOR), performance.getAudience()));
+            totalAmount += getAmount(performance);
         }
         result.append(String.format("Amount owed is %s%n", frmt.format(totalAmount / Constants.PERCENT_FACTOR)));
         result.append(String.format("You earned %s credits%n", volumeCredits));
